2b3c3b3889c9ffcd7311b1b3be3b8d4e66f8e282
Duy Vu <duysp1994@gmail.com>
2021-10-22 09:29:17 +0200

    fix user factory

--- src/database/factories/user.factory.ts
+++ src/database/factories/user.factory.ts
@@ -4,7 +4,7 @@ import { RoleType } from '../../common/constants/role-type';
 import { UserEntity } from '../../modules/user/user.entity';
 
 define(UserEntity, (faker) => {
-    const gender = faker.datatype.number(1);
+    const gender = faker.random.number(1);
     const firstName = faker.name.firstName(gender);
     const lastName = faker.name.lastName(gender);
     const email = faker.internet.email(firstName, lastName);
