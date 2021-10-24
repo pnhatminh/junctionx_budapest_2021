6044fd0acf7dbfafde636b842d6b3f01a8048092
Duy Vu <duysp1994@gmail.com>
2021-10-24 08:47:01 +0200

    AlterTable

--- src/database/migrations/1635027474246AlterDiseaseQuestionnaireTable.ts
+++ src/database/migrations/1635027474246AlterDiseaseQuestionnaireTable.ts
@@ -1,19 +1,24 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import type { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class AlterDiseaseQuestionnaireTable1635027474246 implements MigrationInterface {
-    name = 'AlterDiseaseQuestionnaireTable1635027474246'
+export class AlterDiseaseQuestionnaireTable1635027474246
+  implements MigrationInterface
+{
+  name = 'AlterDiseaseQuestionnaireTable1635027474246';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "diseases" DROP COLUMN "questionnaires_id"`);
-        await queryRunner.query(`ALTER TABLE "questionnaires" ADD "disease_id" integer`);
-        await queryRunner.query(`ALTER TABLE "questionnaires" ADD CONSTRAINT "FK_4ab5c99a66d8528489511a78030" 
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      'ALTER TABLE "questionnaires" ADD "disease_id" integer',
+    );
+    await queryRunner.query(`ALTER TABLE "questionnaires" ADD CONSTRAINT "FK_4ab5c99a66d8528489511a78030" 
         FOREIGN KEY ("disease_id") REFERENCES "diseases"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-    }
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "questionnaires" DROP CONSTRAINT "FK_4ab5c99a66d8528489511a78030"`);
-        await queryRunner.query(`ALTER TABLE "questionnaires" DROP COLUMN "disease_id"`);
-        await queryRunner.query(`ALTER TABLE "diseases" ADD "questionnaires_id" integer`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      'ALTER TABLE "questionnaires" DROP CONSTRAINT "FK_4ab5c99a66d8528489511a78030"',
+    );
+    await queryRunner.query(
+      'ALTER TABLE "questionnaires" DROP COLUMN "disease_id"',
+    );
+  }
 }
